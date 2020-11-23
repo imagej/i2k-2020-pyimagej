705c97cec6a2fc575a3dfb8ae46e746f8da240d2
Mark Hiner <hinerm@gmail.com>
2020-11-23 15:46:22 -0600

    Move to single environment file
    This is way cleaner and simplified (thanks @imagejan!) but currently
    broken.
    
    jpype will not install locally (from pip) without ALSO being listed in
    the standard conda dependencies. This creates an environment that
    appears to be correct, but results in a corrupted scyjava.
    
    Manually re-installing scyjava appears to fix things.

--- setup.bat
+++
@@ -1,1 +0,0 @@
-cd dependencies & conda env create --file environment.yml & conda activate i2k-2020 & easy_install pip & (for /F "usebackq tokens=*" %%A in ("requirements.txt") do pip install -e %%A) & cd ..\notebooks & jupyter notebook
